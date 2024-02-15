ea61f2f47c33b06e3b85ad3d44f2065ed83d7db6
marceltsubota_ <marcel.tsu@hotmail.com>
2024-02-15 15:48:25 -0300

    Melhorias na usabilidade

---
+++ static/scripts.js
@@ -0,0 +1,42 @@
+// Função para atualizar a carteira
+function atualizarCarteira() {
+    fetch('/atualizar_carteira')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('saldo_dolar').innerText = data.saldo_dolar;
+            document.getElementById('saldo_btc').innerText = data.saldo_btc;
+        });
+}
+
+// Adiciona event listener para o input de depósito
+document.getElementById('valor_deposito').addEventListener('change', function() {
+    atualizarCarteira();
+});
+
+// Adiciona event listener para o input de saque
+document.getElementById('valor_saque').addEventListener('change', function() {
+    atualizarCarteira();
+});
+
+// Adiciona event listener para o input de compra de BTC
+document.getElementById('valor_compra_btc').addEventListener('change', function() {
+    atualizarCarteira();
+});
+
+// Adiciona event listener para o input de venda de BTC
+document.getElementById('valor_venda_btc').addEventListener('change', function() {
+    atualizarCarteira();
+});
+
+function consultarValorBTC() {
+    fetch('/consultar_valor_btc_atual')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('valor_btc_atual').innerText = data.valor_btc;
+        })
+        .catch(error => {
+            console.error("Erro ao consultar o valor atual do BTC:", error);
+        });
+}
+consultarValorBTC(); // Consulta ao carregar a página
+setInterval(consultarValorBTC, 11000);
